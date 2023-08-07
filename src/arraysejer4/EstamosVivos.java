b7b42c59b9c100ca9851ce46ce35a1a1859c0944
McGuinnis <141609613+McGuinnis@users.noreply.github.com>
2023-08-07 12:49:09 -0300

    Nnueva clase con un private
    solo agrege una clase mas

---
+++ src/arraysejer4/EstamosVivos.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arraysejer4;
+
+/**
+ *
+ * @author Luis
+ */
+public class EstamosVivos {
+    private int codigo;
+    
+}
