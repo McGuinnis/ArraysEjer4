5ccb0d35741fe8e742b9d6e6a53a7d6dd6f8d81c
Luciano-A1 <141652211+Luciano-A1@users.noreply.github.com>
2023-08-07 12:36:57 -0300

    Modificacion

---
+++ src/clases1/NewMain.java
@@ -0,0 +1,17 @@
+package clases1;
+
+/**
+ *
+ * @author lucia
+ */
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println(">>> Hola Grupo <<<");
+    }
+    
+}
